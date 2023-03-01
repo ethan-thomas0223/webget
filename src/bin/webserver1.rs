79b996a3a02c854bd47256f48c08c2902b5b5273
Ethan Thomas <thomasem@hendrix.edu>
2023-03-01 12:37:31 -0600

    Webget Still Broken, working on Webserver step 2

--- src/bin/webserver1.rs
+++ src/bin/webserver1.rs
@@ -4,6 +4,9 @@ use std::env;
 use std::io; 
 use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, execvp}};
 use check::*;
+use std::{thread, sync::Arc};
+use crossbeam::atomic::AtomicCell;
+
 
 //got TCP Listener framewrok from documentation
 fn main() -> std::io::Result<()> {
@@ -17,5 +20,20 @@ fn main() -> std::io::Result<()> {
 }
 
 fn handle_client(stream: TcpStream) {
-    println!("{}", stream);
+    //println!("{}", stream);
+    thread::spawn(move||{
+        let mut client_msg: &str = "".to_string();
+        let mut end_char = false;
+        while !end_char{
+            let buf = BufReader::new(stream);
+            //need 500 byte limiter
+            let from_bytes = str::from_utf8(&buf).unwrap();
+            let msg = client_msg.read(from_bytes);
+            client_msg.push_str(msg);
+            if client_msg.contains("\r\n\r\n") or client_msg.contains("\n\n"){
+                end_char = true;
+            }
+        }
+        println!("{}", client_msg);
+    })
 }
